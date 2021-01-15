6765aec6a7289e314a336385dc4b79d36e80e6d3
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-01-15 16:11:40 +0700

    Lab11_1 done!! with 102.05 sec

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int x);
+int y = 0;
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(50);
     return 0; 
+}
+
+long long int fibonacci(int x){
+    if(x==1){return 1;} 
+    else if(x==0){return 0;}
+    else{return fibonacci(x-1)+fibonacci(x-2);}
 }
